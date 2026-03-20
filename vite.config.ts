63feafcffb232e3c96591dcd6755d799c2410f9d
root <root@wordpress.hader.ovh>
2026-03-20 19:34:56 +0100

    setup vite & bun

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                booking: resolve(__dirname, 'elements/booking/src/booking.ts'),
+            },
+            output: {
+                entryFileNames: '[name].js',      // → dist/booking.js
+                assetFileNames: '[name][extname]'  // → dist/booking.css
+            }
+        }
+    }
+});
